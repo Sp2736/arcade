d5e6000d8e117ec32b36502fcd2fcdfbc75c8e30
Swayam Patel <swayampatel2736@gmail.com>
2026-02-11 13:31:59 +0530

    Minor change in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,16 @@
-/* import MainLayout from "@/src/components/home/MainLayout";
+// import MainLayout from "@/src/components/home/MainLayout";
 
-export default function Home() {
-  return (
-    <main>
-      <MainLayout />
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <MainLayout />
+//     </main>
+//   );
+// }
 import FacultyDashboard from "@/src/components/dashboard-faculty/FacultyDashboard";
 import StudentDashboard from "@/src/components/dashboard-student/StudentDashboard";
 
 export default function Page() {
   return <StudentDashboard />;
- return <FacultyDashboard/>;
-} */
-
-
- import FacultyDashboard from "@/src/components/dashboard-faculty/FacultyDashboard";
-import StudentDashboard from "@/src/components/dashboard-student/StudentDashboard";
-
-export default function Page() {
-  // Toggle the comment below to switch between dashboards during development
-  return <StudentDashboard />;
-  
-  
+//  return <FacultyDashboard/>;
 }
